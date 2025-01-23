c21254ad1d0278ae6cc980cf7de7c43bf9da8c43
Ignacio-Albornoz <ignaciog.albornoz@gmail.com>
2025-01-23 19:28:12 -0300

    Nuevos icons

--- src/components/historyButton/index.js
+++ src/components/historyButton/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { FaFileExcel, FaSyncAlt, FaDownload } from 'react-icons/fa'; // Ejemplo de íconos
+import { FaSyncAlt, FaWallet  } from 'react-icons/fa'; // Ejemplo de íconos
+import { SiGooglesheets } from "react-icons/si";
+
 import './styles.css';
 
 const URL_EXCEL = 'https://docs.google.com/spreadsheets/d/1rIbAzxMusyS5tTIuU6qTQ0fdyt6vz_JDcZX1foJPN30/edit?gid=648864529#gid=648864529';
@@ -37,7 +39,7 @@ const TradeButtons = ({ onReload }) => {
                 {/* Botón para actualizar cuentas */}
                 <button className="trade-button" onClick={updateWallet}>
                     <span className="button-icon">
-                        <FaSyncAlt />
+                        <FaWallet />
                     </span>
                     <span className="button-text">Actualizar Cuentas</span>
                 </button>
@@ -46,7 +48,7 @@ const TradeButtons = ({ onReload }) => {
                 <a href={URL_EXCEL} target="_blank" rel="noopener noreferrer">
                     <button className="trade-button">
                         <span className="button-icon">
-                            <FaFileExcel />
+                            <SiGooglesheets />
                         </span>
                         <span className="button-text">Abrir Excel</span>
                     </button>
@@ -56,7 +58,7 @@ const TradeButtons = ({ onReload }) => {
             {/* Botón para recargar */}
             <button className="trade-button reload-button" onClick={onReload}>
                 <span className="button-icon">
-                    <FaDownload />
+                    <FaSyncAlt />
                 </span>
                 <span className="button-text">Recargar</span>
             </button>
